ee9f7f7944f8001b491bf9f400c29d51babdc4fc
YMOD <YMODCustoms@gmail.com>
2025-02-10 19:46:08 +0000

    remember key

--- yusallinone.lua
+++ yusallinone.lua
@@ -28,7 +28,7 @@ KeySettings = {
     Subtitle = "locked to trusted ppl only",
     Note = "Discord yusd3v dm for the key ", -- Use this to tell the user how to get a key
     FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-    SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+    SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
     GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
     Key = {"KEY-BaVT3eGNp9uk7ULSAZ"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
 }
