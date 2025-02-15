adf8bb8b5babeef9c2a95086ec42d836acb01b2e
YMOD <YMODCustoms@gmail.com>
2025-02-15 14:17:46 +0000

    update(cord grabber)

--- D3v.lua
+++ D3v.lua
@@ -26,7 +26,7 @@ KeySystem = false, -- Set this to true to use our key system
 KeySettings = {
     Title = "Mic up",
     Subtitle = "locked to trusted ppl only",
-    Note = "Discord yusd3v dm for the key ", -- Use this to tell the user how to get a key
+    Note = "Discord yusd3v dm for the key ", -- Use this to tell the user how to get a key  
     FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
     SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
     GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
@@ -311,7 +311,7 @@ local Button = tab10:CreateButton({
     Name = "Cord Grabber",
     Interact = 'Click',
     Callback = function()
-             loadstring(game:HttpGet("https://pastebin.com/raw/An7ujyKp"))()
+             loadstring(game:HttpGet("https://raw.githubusercontent.com/yusd3v/DEVHUB-RBX/refs/heads/main/cord%20grabber%20%5BDEV%5D.lua"))()
     end,
  })
 
