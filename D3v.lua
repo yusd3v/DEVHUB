8e3e9e0dd3e85703326c61664679044743e3758b
YMOD <YMODCustoms@gmail.com>
2025-02-15 17:30:40 +0000

    update and key system back

--- D3v.lua
+++ D3v.lua
@@ -1,7 +1,7 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-Name = "D3V | MIC UP 🔊",
+Name = "D3V | MIC UP 🔊| .gg/k6ZKwdg4kk",
 Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
 LoadingTitle = "MIC UP 🔊",
 LoadingSubtitle = "by yus",
@@ -18,11 +18,11 @@ ConfigurationSaving = {
 
 Discord = {
     Enabled = true, -- Prompt the user to join your Discord server if their executor supports it
-    Invite = "uwu", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+    Invite = "k6ZKwdg4kk", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
     RememberJoins = true -- Set this to false to make them join the discord every time they load it up
 },
 
-KeySystem = false, -- Set this to true to use our key system
+KeySystem = true, -- Set this to true to use our key system
 KeySettings = {
     Title = "Mic up",
     Subtitle = "locked to trusted ppl only",
@@ -30,7 +30,7 @@ KeySettings = {
     FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
     SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
     GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-    Key = {"w"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+    Key = {"UWUKITTY"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
 }
 })
 local tab1 = Window:CreateTab("Local Player", nil) -- Title, Image
